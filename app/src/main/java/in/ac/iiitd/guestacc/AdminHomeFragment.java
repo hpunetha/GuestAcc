d296381e83275780fa20ddffcdd03509d77b628c
kuldeepsngh77 <kuldeepsngh77@gmail.com>
2018-04-18 00:17:53 +0530

    AdminHome added

--- app/src/main/java/in/ac/iiitd/guestacc/AdminHomeFragment.java
+++ app/src/main/java/in/ac/iiitd/guestacc/AdminHomeFragment.java
@@ -1,15 +1,31 @@
 package in.ac.iiitd.guestacc;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
 
 /**
  * Created by kd on 15/4/18.
@@ -19,6 +35,7 @@ public class AdminHomeFragment extends Fragment {
 
     CardView adminHomeCardView;
     TextView mTextViewBookedRooms, mTextViewAvailableRooms;
+    DatabaseReference mFireBaseReference;
 
     //Database
 
@@ -30,7 +47,8 @@ public class AdminHomeFragment extends Fragment {
         mTextViewBookedRooms = (TextView)adminHomeView.findViewById(R.id.adminBookedRooms);
         mTextViewAvailableRooms = (TextView)adminHomeView.findViewById(R.id.adminRoomsAvailable);
 
-
+        //Thread for getting status for today - admin home screen
+        new getStatusForToday().execute("");
 
         adminHomeCardView = (CardView) adminHomeView.findViewById(R.id.adminHomeCardView1);
 
@@ -71,4 +89,55 @@ public class AdminHomeFragment extends Fragment {
 
         return adminHomeView;
     }
+
+    @SuppressLint("StaticFieldLeak")
+    private class getStatusForToday extends AsyncTask<String,String,String>{
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            mFireBaseReference = FirebaseDatabase.getInstance().getReference("bookings");
+            //https://stackoverflow.com/questions/8654990/how-can-i-get-current-date-in-android
+            final String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+            HashSet<String> bookingsRooms = new HashSet<>(Arrays.asList("bh1","bh2","gh1","gh2",
+                    "fr1","fr2"));
+            //Log.i("HashsetSize", String.valueOf(bookingsRooms.size()));
+
+            //Log.i("data",date);
+            mFireBaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Log.i("DataSnapshot", String.valueOf(dataSnapshot.getChildrenCount()));
+                    for (DataSnapshot bookingData: dataSnapshot.getChildren()){
+                        if (bookingData.getKey().equals("2018-03-17")){
+                            //for (bookingData.getKey().)
+                            Log.i("Data", String.valueOf(bookingData.getChildrenCount()));
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            return null;
+        }
+    }
+
+/*
+    @SuppressLint("StaticFieldLeak")
+    class getStatusForToday extends AsyncTask<void,void,void>{
+
+
+        @Override
+        protected void doInBackground(void... voids) {
+
+        }
+    }*/
 }
