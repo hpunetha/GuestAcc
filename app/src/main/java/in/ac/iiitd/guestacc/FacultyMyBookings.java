abcf4e60caa66a2cd05f6ce714dff058a7854b08
hpunetha <himanshu17016@iiitd.ac.in>
2018-04-30 19:24:41 +0530

    Changes in Verify Payment

---
+++ app/src/main/java/in/ac/iiitd/guestacc/FacultyMyBookings.java
@@ -0,0 +1,102 @@
+package in.ac.iiitd.guestacc;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacultyMyBookings extends AppCompatActivity implements FacultyMyBookings_RecyclerAdapter.ValidateItemClickListener
+{
+
+
+    RecyclerView recyclerView ;
+    FacultyMyBookings_RecyclerAdapter adapter ;
+    DatabaseReference mFireBaseReference;
+    List<Admin_Data_ValidatePayment> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_validate_payment);
+
+
+        ProgressDialog progress = new ProgressDialog(this);
+        progress.setMessage("Loading...Please Wait ");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        //progress.show();
+       // progress.dismiss();
+
+        data = new ArrayList<>();
+        mFireBaseReference = FirebaseDatabase.getInstance().getReference("pending_requests/verify_payment/");
+
+        mFireBaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                Admin_Data_ValidatePayment mAdminData ;
+
+                for (DataSnapshot ds2: dataSnapshot.getChildren())
+                {
+                    if (ds2!=null) {
+
+                        mAdminData = new Admin_Data_ValidatePayment();
+                        mAdminData.setReqId(ds2.getKey());
+                        mAdminData.setPersons(ds2.child("no_of_persons").getValue().toString());
+                        mAdminData.setRooms(ds2.child("no_of_rooms").getValue().toString());
+                        mAdminData.setTotal(ds2.child("total_price").getValue().toString());
+                        Log.i("madmindata", mAdminData.getPersons() + " " + mAdminData.getReqId() +" " + mAdminData.getRooms() + " " + mAdminData.getTotal());
+                        data.add(mAdminData);
+                    }
+                }
+
+                if(adapter!=null) { adapter.notifyDataSetChanged();}
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+//
+//        for(int i=0;i<20;i++)
+//        {
+//            data.add(new Admin_Data_ValidatePayment()) ;
+//        }
+
+
+        recyclerView = (RecyclerView)findViewById(R.id.validate_payment_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new FacultyMyBookings_RecyclerAdapter(this, data);
+
+        adapter.setClickListener(this);
+
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onItemClick(View v,int position)
+    {
+
+    }
+
+}
